da4efe0c319d4838b6f56f99f39b2f71a56421b8
jihoon_yang <jihoon_yang@ksmartech.com>
2026-07-02 13:50:34 +0900

    fix: modify run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker run --rm -it -p 8080:8080 easy-k8s-yaml 
-
+# 호스트의 Docker 소켓과 임시 디렉터리(/tmp)를 볼륨 바인딩하여 
+# 컨테이너 내부에서 Docker-out-of-Docker(DooD)가 가능하도록 실행합니다.
+docker run --rm -it \
+  -p 8080:8080 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /tmp:/tmp \
+  easy-k8s-yaml
